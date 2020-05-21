8ce9465ff5235e00ea7879c012016a2bc75ac459
Zendau <cowalyow2012@gmail.com>
2020-05-21 19:21:22 +0300

    make it better

--- application/config/db.php
+++ application/config/db.php
@@ -1,8 +1,17 @@
 <?php
 
+// return [
+// 	'host' => 'localhost',
+// 	'name' => 'websz',
+// 	'user' => 'user',
+// 	'password' => '123',
+// ];
+
 return [
 	'host' => 'eu-cdbr-west-03.cleardb.net',
 	'name' => 'heroku_0b2930147418d32',
 	'user' => 'b717a8f70bc512',
 	'password' => '6a8f00c6',
 ];
+
+?>
