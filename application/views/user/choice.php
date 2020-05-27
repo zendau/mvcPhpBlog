a6f310807e1ccb5068a4e3948c87f041e573046d
Zendau <cowalyow2012@gmail.com>
2020-05-27 22:44:19 +0300

    fix ? substr tour and font size

--- application/views/user/choice.php
+++ application/views/user/choice.php
@@ -12,7 +12,7 @@
             <h3 class="section-choice__title"><?php echo $value['title'] ?></h3>
             <img src="../../../public/img/tour/<?php echo explode(" ",$value['img'])[0] ?>"  alt="" class="section-choice__logo">
             <?php if(strlen($value['body']) > 200): ?>
-            <p class="section-choice__text"><?php echo substr($value['body'], 0, 200).'...'; ?></p>
+            <p class="section-choice__text"><?php mb_internal_encoding('UTF-8'); echo mb_substr($value['body'], 0, 200).'...'; ?></p>
             <?php else: ?>
             <p class="section-choice__text"><?php echo $value['body']; ?></p>
             <?php endif; ?>
